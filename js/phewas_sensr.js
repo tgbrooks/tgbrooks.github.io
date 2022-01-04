5a29ac1d2573cf4bc7dc8f73627e9dfbdfbf30ae
Thomas Brooks <tgbrooks@gmail.com>
2022-01-04 14:38:43 -0500

    Elaborate the results section

--- js/phewas_sensr.js
+++ js/phewas_sensr.js
@@ -5,6 +5,12 @@ const app = new Vue({
         phecodes: [],
         three_components_results: null,
         predictive_tests: null,
+        config: {
+            three_components: {
+                x_scale: 1000,
+            },
+        },
+        redraw_plots: true,
     },
 
     computed: {
@@ -38,15 +44,25 @@ const app = new Vue({
             });
     },
 
+    updated: function() {
+        if (this.redraw_charts) {
+            this.plot_results();
+            this.redraw_charts = false;
+        }
+    },
+
     methods: {
         fmt: function(num) {
             // Format numbers for easy visibility
+            if (num === null){ return 'N/A'; }
             return num.toLocaleString('en-US', {maximumSignificantDigits: 3});
         },
         fmt_p: function(num) {
             // Format p-value to just 2 sig-figs
             if (num > 0.01) {
                 return num.toLocaleString('en-US', {maximumSignificantDigits: 3});
+            } else if (num === null) {
+                return 'N/A';
             } else {
                 return num.toExponential(2);
             }
@@ -60,6 +76,83 @@ const app = new Vue({
         },
         fmt_range: function(lower, upper) {
             return "(" + this.fmt(lower) + " - " + this.fmt(upper) + ")";
+        },
+
+        plot_results: function() {
+            // Three components plot
+            let three_comps = this.phenotype_results.three_components;
+            let plot = new Plotly.newPlot( "three_components_chart",
+            [
+                {
+                    x: [Math.abs(three_comps.physical_eff)],
+                    y: [2],
+                    mode: 'markers',
+                    type: 'scatter',
+                    name: 'Physical Activity',
+                    marker: {size: 12, color: "red"},
+                },
+                {
+                    x: [Math.abs(three_comps.sleep_eff)],
+                    y: [1],
+                    mode: 'markers',
+                    type: 'scatter',
+                    name: 'Sleep',
+                    marker: {size: 12, color: "blue"},
+                },
+                {
+                    x: [Math.abs(three_comps.circ_eff)],
+                    y: [0],
+                    mode: 'markers',
+                    type: 'scatter',
+                    name: 'Circadian Rhythm',
+                    marker: {size: 12, color: "green"},
+                },
+
+                // Error bars:
+                {
+                    x: [Math.abs(three_comps.physical_eff) - 1.96*three_comps.physical_se, Math.abs(three_comps.physical_eff) + 1.96*three_comps.physical_se],
+                    y: [2, 2],
+                    mode: 'lines',
+                    type: 'scatter',
+                    line: {color: "red"},
+                    showlegend: false,
+                },
+                {
+                    x: [Math.abs(three_comps.sleep_eff) - 1.96*three_comps.sleep_se, Math.abs(three_comps.sleep_eff) + 1.96*three_comps.sleep_se],
+                    y: [1, 1],
+                    mode: 'lines',
+                    type: 'scatter',
+                    line: {color: "blue"},
+                    showlegend: false,
+                },
+                {
+                    x: [Math.abs(three_comps.circ_eff) - 1.96*three_comps.circ_se, Math.abs(three_comps.circ_eff) + 1.96*three_comps.circ_se],
+                    y: [0, 0],
+                    mode: 'lines',
+                    type: 'scatter',
+                    line: {color: "green"},
+                    showlegend: false,
+                },
+            ],
+            {
+                xaxis: {
+                    range: [0.0,0.3],
+                    title: "Effect size",
+                },
+                yaxis: {
+                    showline: false,
+                    showgrid: false,
+                    showticklabels: false,
+                    zeroline: false,
+                }
+            },
+            {staticPlot:true});
+        },
+    },
+
+    watch: {
+        phenotype_results : function() {
+            this.redraw_charts = true;
         },
     },
 });
